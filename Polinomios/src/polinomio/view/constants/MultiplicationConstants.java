df077f3cfa067f3e44da278c40444ff1f5181b29
Juan Arismendy <juand16@gmail.com>
2021-04-17 11:19:16 -0500

    _

--- Polinomios/src/polinomio/view/constants/MultiplicationConstants.java
+++ Polinomios/src/polinomio/view/constants/MultiplicationConstants.java
@@ -3,7 +3,7 @@ public class MultiplicationConstants {
     public static final String TITLE =
     "\n" + "[!] Interfaz de creación de multiplicación ";
     public static final String SUBTITLE = 
-    "[*]  ";
+    "[*] gt ";
     public static final String NOTES = "[*] ";
     public static final String QUESTION = 
     "[?] Ingresar otro polinomio? (y/n) : ";
