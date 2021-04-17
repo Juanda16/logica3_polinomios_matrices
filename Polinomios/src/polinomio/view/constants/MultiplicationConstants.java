b828f2c1f494d624163490587f29cec14b923a57
Juan Arismendy <juand16@gmail.com>
2021-04-17 11:10:28 -0500

    sort done

---
+++ Polinomios/src/polinomio/view/constants/MultiplicationConstants.java
@@ -0,0 +1,11 @@
+
+public class MultiplicationConstants {
+    public static final String TITLE =
+    "\n" + "[!] Interfaz de creación de multiplicación ";
+    public static final String SUBTITLE = 
+    "[*] Ingrese el polinimio usando el siguiente ejemplo: \n 5X^3 + 2X^2 + 10X + 1 ";
+    public static final String NOTES = "[*] ";
+    public static final String QUESTION = 
+    "[?] Ingresar otro polinomio? (y/n) : ";
+
+}
--- Polinomios/src/polinomio/view/constants/NewPolinomioConstants.java
+++ Polinomios/src/polinomio/view/constants/NewPolinomioConstants.java
@@ -11,3 +11,4 @@ public class NewPolinomioConstants {
     public static final String NOTES= "[*] ";
     
 }
+    
